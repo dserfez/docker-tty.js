a7b0b2756caf9e9bd45c9a8d9f02d57558929606
Davor Serfez <dserfez@gmail.com>
2016-02-26 14:26:40 +0100

    start script fix

--- start_script.sh
+++ start_script.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+### BEGIN CONFIG CESTION
+
+DOCKER_OPTS="-ti"
+
+: ${SSH_HOST:=172.17.42.1}
+: ${SSH_USER:=core}
+
 : ${USER_HOME:=/home/core}
+
+### end CONFIG CESTION
 
 KEY_FILE="${USER_HOME}/.ssh/id_rsa"
 
@@ -20,9 +29,12 @@ genkeys() {
 
 #docker run -ti -p 8080:8080 -h tty --rm --name tty -v ${KEY_FILE}:/home/core/.ssh/id_rsa -e HOME=/home/core ttyjs #/bin/sh
 #docker run --rm --name ttyjs -h ttyjs -v ${KEY_FILE}:/home/core/.ssh/id_rsa -e HOME=/home/core cycomf/ttyjs
-docker run --rm --name ttyjs -h ttyjs \
+docker run $DOCKER_OPTS --rm --name ttyjs -h ttyjs \
   -v /home/core/.ssh:/home/core/.ssh \
+  -e SSH_HOST=${SSH_HOST} \
+  -e SSH_USER=${SSH_USER} \
   -u core \
   cycomf/ttyjs
 
 [ ${ADDED_TO_AH} == "true" ] && sed -i 's|^.*tty.js-host$||' ${USER_HOME}/.ssh/authorized_keys
+
